6b903c2f2da54dd2a35a1bb4d3a02bd88293c4f4
uriybashkirov <Uriybashkirov@gmail.com>
2016-10-18 13:02:00 +0300

    Update Вариант 69.py

--- Labor2/Вариант 69.py
+++ Labor2/Вариант 69.py
@@ -1,30 +1,30 @@
 import math
 # -*- coding: utf-8 -*- 
 
-print ( ' Введите x = ')
-x = int(input ( ) )
+print ('Введите x = ')
+x = int(input())
 
-print ( ' Введите a = ' )
-a = int(input ( ) )
+print ('Введите a = ' )
+a = int(input())
 
-print ( ' Введите:N = 1 для нахождения  переменной G. N = 2 для нахождения  переменной F. N = 3 для нахождения переменной Y. ' )
-n = int(input ( ) )
+print ('Введите: N = 1 для нахождения  переменной G. N = 2 для нахождения  переменной F. N = 3 для нахождения переменной Y.')
+n = int(input())
 
-if  n == 1:
-    Y = ( ( 9 * ( 7 *a * a + 39 * a * x + 20 * x * x) ) / (9 * a * a + 59 * a * x + 30 * x * x) )
+if n == 1:
+    Y = ((9 *(7 *a * a + 39 * a * x + 20 * x * x)) / (9 * a * a + 59 * a * x + 30 * x * x))
     if (0 <= Y) or (Y >= 0):
-        print( round ( Y, 3 ) )
+        print(round(Y, 3))
     else:
         print ('Y не может быть результатом деления на 0.')
 elif n == 2:
-    F = math.tanh( 9 * a * a - 14 * a * x + 5 * x * x) ;
-    if  F:
-        print( round ( F, 3 ) );
+    F = math.tanh(9 * a * a - 14 * a * x + 5 * x * x)
+    if F:
+        print(round(F, 3))
 elif n == 3:
-    G = math.log ( 14 * a * a - 75 * a * x + 54 * x * x + 1) / log(10)
+    G = math.log(14 * a * a - 75 * a * x + 54 * x * x + 1) / math.log(10)
     if G >= 0:
-        print( round ( G, 3 ) )
+        print(round(G, 3))
     else:
-        print( ' Логарифм не может быть найден при таких значениях переменных. ' )
-    else:
-        print( ' Ошибка, попросите вывод результата одной из переменных G,F или Y. ' )
+        print('Логарифм не может быть найден при таких значениях переменных.')
+else:
+    print('Ошибка, попросите вывод результата одной из переменных G,F или Y.')
