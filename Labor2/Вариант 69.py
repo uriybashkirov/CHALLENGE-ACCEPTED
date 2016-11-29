ec10b28df741ff00ffbbb4510f8ae396510f437d
uriybashkirov <Uriybashkirov@gmail.com>
2016-11-29 20:03:11 +0300

    Update Вариант 69.py

--- Labor2/Вариант 69.py
+++ Labor2/Вариант 69.py
@@ -11,19 +11,18 @@ print ('Введите: N = 1 для нахождения  переменной 
 n = int(input())
 
 if n == 1:
-    Y = ((9 *(7 *a * a + 39 * a * x + 20 * x * x)) / (9 * a * a + 59 * a * x + 30 * x * x))
-    if (0 <= Y) or (Y >= 0):
-        print(round(Y, 3))
+    G = ((9 *(7 *a * a + 39 * a * x + 20 * x * x)) / (9 * a * a + 59 * a * x + 30 * x * x))
+    if (0 >= G) or (G >= 0):
+        print(round(G, 3))
     else:
-        print ('Y не может быть результатом деления на 0.')
+        print ('G не может быть результатом деления на 0.')
 elif n == 2:
     F = math.tanh(9 * a * a - 14 * a * x + 5 * x * x)
-    if F:
-        print(round(F, 3))
+    print(round(F, 3))
 elif n == 3:
-    G = math.log(14 * a * a - 75 * a * x + 54 * x * x + 1) / math.log(10)
-    if G >= 0:
-        print(round(G, 3))
+    Y = math.log(14 * a * a - 75 * a * x + 54 * x * x + 1) / math.log(10)
+    if Y >= 0:
+        print(round(Y, 3))
     else:
         print('Логарифм не может быть найден при таких значениях переменных.')
 else:
